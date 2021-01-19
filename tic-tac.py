b80c9a12d8d9496e424dafe9e391475e8f17a380
dlyapun <dlyapun@gmail.com>
2021-01-19 13:09:27 +0200

    Improve to OOP based

--- tic-tac.py
+++ tic-tac.py
@@ -1,104 +1,124 @@
 import os
-os.system("clear")
+import sys
+os.system('clear')
 
 
-class Board():
-    def __init__(self):
-        self.cells = [[" ", " ", " "],
-                      [" ", " ", " "], 
-		              [" ", " ", " "]]
-                      
+class Board:
+
+    def __init__(self, *args, **kwargs):
+        """Initial board instance. """
+
+        self.cells = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
+
     def display(self):
-        print((" %s | %s | %s " %(self.cells[0][0], self.cells[0][1], self.cells[0][2])))
-        print("-----------")
-        print((" %s | %s | %s " %(self.cells[1][0], self.cells[1][1], self.cells[1][2])))
-        print("-----------")
-        print((" %s | %s | %s " %(self.cells[2][0], self.cells[2][1], self.cells[2][2])))
+        print((' %s | %s | %s ' %(self.cells[0][0], self.cells[0][1], self.cells[0][2])))
+        print('-----------')
+        print((' %s | %s | %s ' %(self.cells[1][0], self.cells[1][1], self.cells[1][2])))
+        print('-----------')
+        print((' %s | %s | %s ' %(self.cells[2][0], self.cells[2][1], self.cells[2][2])))
 
     def update_cell(self, row, column, player):
         if self.cells[row][column] == " ":
             self.cells[row][column] = player
 
     def is_winner(self, player):
-        x_or_y = ''.join(player+player+player)
-        if ''.join(self.cells[0]) == x_or_y \
-            or ''.join(self.cells[1]) == x_or_y\
-            or ''.join(self.cells[2]) == x_or_y\
-            or (self.cells[0][0] == self.cells[1][1] == self.cells[2][2] == player)\
-            or (self.cells[0][2] == self.cells[1][1] == self.cells[2][0] == player):
-            result = True
-            if result:
+        winner = player*3  # 'XXX' or 'OOO'
+
+        # Проверяем строки
+        for cell in self.cells:
+            if ''.join(cell) == winner:
                 return True
-        return False        
+
+        # Мы развернули нашу матрицу на 90 градусов, и тпперь проверяем столбцы.
+        for cell in list(zip(*self.cells)):
+            if ''.join(cell) == winner:
+                return True
+
+        # Смотрим первую диагональ
+        if self.cells[0][0] + self.cells[1][1] + self.cells[2][2] == winner:
+            return True
+
+        # Смотрим вторую диагональ
+        if self.cells[0][2] + self.cells[1][1] + self.cells[2][0] == winner:
+            return True
+
+        return False
+
 
     def is_tie(self):
         for row in self.cells:
             for element in row:
-                if element == " ":
+                if element == ' ':
                     return False
-        return True          
-                    
-        
+        return True
+
     def reset(self):
-        self.cells = [[" ", " ", " "],
-                      [" ", " ", " "], 
-		              [" ", " ", " "]] 
+        """Reset board cells. """
+
+        self.cells = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
 
 
-board = Board()
+class Game:
+    """Main game tic-tac class. """
 
+    def __init__(self, board, *args, **kwargs):
+        """Initial game instance. """
 
-def print_header():
-    print("Welcom to Tic-Tac-Toe\n")
+        self.board = board
 
+    def print_header(self):
+        print('Welcom to Tic-Tac-Toe\n')
 
-def refresh_screen():
-    os.system("clear")
-    print_header()
-    board.display()
+    def refresh_screen(self):
+        os.system('clear')
+        self.print_header()
+        self.board.display()
 
+    def run(self):
+        while True:
+            self.refresh_screen()
+            self.step('X')
+            self.refresh_screen()
+            if self.board.is_winner('X'):
+                print('\nX wins!\n')
+                self.finish()
 
-while True:
-    refresh_screen()
-    x_row = (int(eval(input("\nX) Plase choose row >"))-1))
-    x_column = (int(eval(input("\nX) Plase choose column >"))-1))
-    board.update_cell(x_row, x_column, "X")
-    refresh_screen()
-    if board.is_winner("X"):
-        print("\nX wins!\n")
-        play_again = input("Would you like to play again? (Y/N) > ").upper()
-        if play_again == "Y":
-            board.reset()
-            continue
+            if self.board.is_tie():
+                print('\nTie game!\n')
+                self.finish()
+
+            self.step('O')
+            if self.board.is_winner('O'):
+                print('\nO wins!\n')
+                self.finish()
+
+            if self.board.is_tie():
+                print('\nTie game!\n')
+                self.finish()
+
+    def step(self, player):
+        """One game step. Select row, column and update cell. """
+
+        selected_row = int(
+            input('\n{0}) Please choose row >'.format(player))
+        )-1
+        print('selected_row: ', selected_row, type(selected_row))
+        selected_column = int(
+            input('\n{0}) Please choose column >'.format(player))
+        )-1
+        self.board.update_cell(selected_row, selected_column, player)
+
+    def finish(self):
+        play_again = input('Would you like to play again? (Y/N) > ').upper()
+        if play_again == 'Y':
+            self.board.reset()
         else:
-            break
+            sys.exit()
 
-    if board.is_tie():
-        print("\nTie game!\n")
-        play_again = input("Would you like to play again? (Y/N) > ").upper()
-        if play_again == "Y":
-            board.reset()
-            continue
-        else:
-            break    
 
-    o_row = (int(eval(input("\nO) Plase choose row >"))-1))
-    o_column = (int(eval(input("\nO) Plase choose column >"))-1))
-    board.update_cell(o_row, o_column, "O")
-    if board.is_winner("O"):
-        print("\nO wins!\n")
-        play_again = input("Would you like to play again? (Y/N) > ").upper()
-        if play_again == "Y":
-            board.reset()
-            continue
-        else:
-            break
+if __name__ == "__main__":
+    # Your code start here
 
-    if board.is_tie():
-        print("\nTie game!\n")
-        play_again = input("Would you like to play again? (Y/N) > ").upper()
-        if play_again == "Y":
-            board.reset()
-            continue
-        else:
-            break      
+    board = Board()
+    game = Game(board)
+    game.run()
